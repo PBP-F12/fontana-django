c742a0a2a24daaeb73c94228b0ea10560f953915
Arxeno <rmfbkw04@gmail.com>
2023-10-04 02:53:18 +0700

    initial commit

---
+++ bookshelve_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bookshelve_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookshelve_django.settings')
+
+application = get_wsgi_application()
