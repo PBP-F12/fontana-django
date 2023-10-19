8639634a335fceb9fc989780b4685fdee3573e72
Arxeno <rmfbkw04@gmail.com>
2023-10-19 23:01:11 +0700

    fix: add module auths and change AUTH_USER_MODEL

--- bookshelve_django/settings.py
+++ bookshelve_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'auths'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'auths.User'
