cdf5577ad941c23c669ba230735dad57f33c12dc
Arxeno <rmfbkw04@gmail.com>
2023-10-24 03:08:38 +0700

    create Author model

--- auths/models.py
+++ auths/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.db.models.query import QuerySet
 
 # Create your models here.
 
@@ -18,3 +20,18 @@ class User(AbstractUser):
         if not self.pk:  # if user doesn't have a primary key, or if user hasn't been created then...
             self.role = self.base_role
             return super().save(*args, **kwargs)
+
+
+class AuthorManager(BaseUserManager):
+    def get_queryset(self, *args, **kwargs) -> QuerySet:
+        result = super().get_queryset(*args, **kwargs)
+        return result.filter(role=User.Role.AUTHOR)
+
+
+class Author(User):
+    base_role = User.Role.AUTHOR
+
+    author = AuthorManager()
+
+    class Meta:
+        proxy = True  # prevent generate class table in database
