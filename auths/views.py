d4675696887f5939ea3bb934a98873f79e4a6305
Arxeno <rmfbkw04@gmail.com>
2023-10-24 16:04:36 +0700

    feat: add register link as another role

--- auths/views.py
+++ auths/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib import messages
 from .forms import AuthorRegistrationForm, ReaderRegistrationForm
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -17,7 +18,12 @@ def register_as_reader(request):
             messages.success(
                 request, 'Your account has been successfully created!')
             return redirect('auths:login')
-    context = {'role': 'Reader', 'form': form}
+    context = {
+        'role': 'Reader',
+        'form': form,
+        'register_as_another_role_href': '/auth/register/author',
+        'another_role': 'Author'
+    }
     return render(request, 'register.html', context)
 
 
@@ -31,9 +37,21 @@ def register_as_author(request):
             messages.success(
                 request, 'Your account has been successfully created!')
             return redirect('auths:login')
-    context = {'role': 'Author', 'form': form}
+    context = {'role': 'Author', 'form': form, 'register_as_another_role_href': '/auth/register/reader',
+               'another_role': 'Reader'}
     return render(request, 'register.html', context)
 
 
 def login_user(request):
-    return HttpResponse('login user')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(
+                request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
