a0989c55183c41039d015cb23c168064182fa33b
Arxeno <rmfbkw04@gmail.com>
2023-10-24 15:58:31 +0700

    feat(auths): add name in urls.py

--- auths/urls.py
+++ auths/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/reader/', views.register_as_reader),
-    path('register/author/', views.register_as_author),
-    path('login/', views.login_user)
+    path('register/reader/', views.register_as_reader, name='register_as_reader'),
+    path('register/author/', views.register_as_author, name='register_as_author'),
+    path('login/', views.login_user, name='login')
 ]
