5aa803c4d4898e6cb0aab476fa40c540297c9e74
Arxeno <rmfbkw04@gmail.com>
2023-10-27 01:29:18 +0700

    finish main page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Book
+
 # Create your views here.
 
 
 @login_required(login_url='auth/login')
 def show_main(request):
-    return render(request, 'main.html')
+    if request.user == 'AUTHOR':
+        books = Book.objects.filter(author_id=request.user)
+    else:
+        books = Book.objects.all()
+
+    context = {'role': request.user.role, 'books': books}
+    return render(request, 'main.html', context)
