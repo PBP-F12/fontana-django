40d0c97f2b0bfd8f40b8e0a4ebf11bcff6f400a3
ilhamzik <zikriilham2@gmail.com>
2023-10-27 18:20:11 +0700

    bookmark

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from bookmark.views import add_bookmark, delete_bookmark, get_bookmark_by_user
+from bookmark.views import add_bookmark, delete_bookmark, get_bookmark_by_user, delete_bookmark_by_book_id
 
 app_name = 'bookmark'
 
 urlpatterns = [
     path('add/<str:book_id>', add_bookmark, name='add_bookmark'),
-    path('delete/', delete_bookmark, name='delete_bookmark'),
+    path('delete/<str:bookmark_id>', delete_bookmark, name='delete_bookmark'),
+    path('delete/book/<str:book_id>', delete_bookmark_by_book_id, name='delete_bookmark_by_book_id'),
     path('', get_bookmark_by_user, name='get_bookmark_by_user'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from bookmark.models import Bookmark
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from main.models import Book
 
 def add_bookmark(request, book_id):
@@ -12,18 +13,26 @@ def add_bookmark(request, book_id):
     new_bookmark = Bookmark(user_id=request.user, book_id=book_bookmarked)
     new_bookmark.save()
 
-    return HttpResponse('add bookmark success')
+    return HttpResponseRedirect(reverse('main:show_main'))
 
 
 
 def delete_bookmark(request, bookmark_id):
     bookmark = get_object_or_404(Bookmark, bookmark_id=bookmark_id)
     bookmark.delete()
+    return HttpResponseRedirect(reverse('bookmark:get_bookmark_by_user'))
+
+def delete_bookmark_by_book_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    bookmark = Bookmark.objects.get(user_id=request.user, book_id=book)
+
+    bookmark.delete()
     return HttpResponseRedirect(reverse('main:show_main'))
+
 
 def get_bookmark_by_user(request):
     bookmark = Bookmark.objects.filter(user_id=request.user)
-    context = {'bookmarks': bookmark}
+    context = {'bookmarks': bookmark, 'role': request.user.role}
 
     return render(request, 'my_bookmark.html', context)
 # Create your views here.
