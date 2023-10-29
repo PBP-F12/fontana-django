d3dfc6f938fa4fa0bd8a8bc44bbf7dfb8a125783
raisahzr <raisa.hannaliya@gmail.com>
2023-10-29 21:11:37 +0700

    event details, create event clear

--- event/views.py
+++ event/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse, reverse_lazy
@@ -17,28 +17,41 @@ def list_event(request):
     context = {'events': event, 'books' : book}
     return render(request, "list_event.html", context)
 
+
 @login_required(login_url=reverse_lazy('auths:login')) 
 def get_event_details(request, event_id):
     event = Event.objects.get(pk=event_id)
     context = {'event': event}
     return render(request, "event_details.html", context)
 
+
 @login_required(login_url=reverse_lazy('auths:login')) 
+@csrf_exempt
 def publish_event(request):
     if request.user.role != 'ADMIN':
         return HttpResponseForbidden('FORBIDDEN')
 
     form = EventForm(request.POST or None)
-
-    if form.is_valid() and request.method == 'POST':
-        forum = form.save(commit=False)
-        # forum.book_id = request.user
-        forum.save()
+    if request.method == 'POST':
+        form = EventForm(request.POST or None)
+        event_name = request.POST.get("event_name")
+        description = request.POST.get("description")
+        poster_link = request.POST.get("poster_link")
+        location = request.POST.get("location")
+        event_date = request.POST.get("event_date")
+        book_id = request.POST.get("book_id")
+        book = get_object_or_404(Book, book_id=book_id)
+        
+        event = Event(event_name=event_name, description=description, poster_link=poster_link, location=location, event_date=event_date, book_id=book)
+        event.save()
 
         return HttpResponseRedirect(reverse('event:list_event'))
+    else:
+        form = EventForm()
 
     context = {'form': form}
     return render(request, "create_event.html", context)
+
 
 @login_required(login_url=reverse_lazy('auths:login'))
 def list_event_ajax(request):
