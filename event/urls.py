5658f1d4c27d41cbf7a5c9de07049c8ba859f230
raisahzr <raisa.hannaliya@gmail.com>
2023-10-29 18:10:34 +0700

    (-)ajax

--- event/urls.py
+++ event/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', list_event, name='list_event'),
     path('publish_event/', publish_event, name='publish_event'),
-    path('<str:event_id>/', get_event_details, name='get_event_details')
+    path('<str:event_id>/', get_event_details, name='get_event_details'),
+    path('ajax/', list_event_ajax, name='list_event_ajax'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse, reverse_lazy
+from yaml import serialize
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Event
+from main.models import Book
 from .forms import EventForm
 
 # Create your views here.
 @login_required(login_url=reverse_lazy('auths:login')) 
 def list_event(request):
     event = Event.objects.all()
-    context = {'events': event}
+    book = Book.objects.all()
+    context = {'events': event, 'books' : book}
     return render(request, "list_event.html", context)
 
 @login_required(login_url=reverse_lazy('auths:login')) 
@@ -35,3 +39,23 @@ def publish_event(request):
 
     context = {'form': form}
     return render(request, "create_event.html", context)
+
+@login_required(login_url=reverse_lazy('auths:login'))
+def list_event_ajax(request):
+    if request.user.role != 'ADMIN':
+        return HttpResponseForbidden('FORBIDDEN')
+
+    events = Event.objects.all()
+    json_response = []
+
+    for event in events:
+        event_json = {
+            'eventDetailsUrl': f'/event/{event.event_id}',
+            'eventName': event.event_name,
+            'posterLink': event.poster_link,
+            'eventDate': event.event_date
+        }
+
+        json_response.append(event_json)
+
+    return JsonResponse({'events': json_response})
