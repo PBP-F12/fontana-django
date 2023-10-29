27e78eb067e6330029ca5b98ac057eb4ccabc65e
ratunadya <nadyaanjania54@gmail.com>
2023-10-29 21:22:57 +0700

    changed book_title to book_id

--- review/urls.py
+++ review/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<str:book_title>/', book_details, name='book_details'),
+    path('<str:book_id>/', book_details, name='book_details'),
     path('', show_mainbruh, name='show_main'),
-    path('review/<str:book_title>/', get_review_json, name='get_review_json'),
-    path('review/add/<str:book_title>/', create_review, name='create_review'),
+    path('review/<str:book_id>/', get_review_json, name='get_review_json'),
+    path('review/add/<str:book_id>/', create_review, name='create_review'),
 ]
