5813f2306cb3170e78ca2547ac721e7c6566020a
MikhailJBS <mikhailharitz@gmail.com>
2023-12-19 11:05:52 +0700

    Added Review For Flutter

--- review/urls.py
+++ review/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('review/<uuid:book_id>/', get_review_json, name='get_review_json'),
     path('review/add/<uuid:book_id>/', create_review, name='create_review'),
     path('delete_review/<int:review_id>', delete_review, name='delete_review'),
+    path('review/addflutter/<uuid:book_id>/', create_review_flutter, name='create_review_flutter'),
 ]
