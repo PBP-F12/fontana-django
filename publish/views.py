aa5bbdb7d6398f4c1410ad5257492df0d2c51bed
ratunadya <nadyaanjania54@gmail.com>
2023-12-20 20:44:00 +0700

    feat: publish flutter django

--- publish/views.py
+++ publish/views.py
@@ -1,6 +1,7 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden, HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseForbidden, HttpResponseRedirect, HttpResponse, JsonResponse
 from django.core import serializers
 from django.urls import reverse, reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
@@ -63,3 +64,42 @@ def get_book_by_author_json(request):
 
     # Create an HTTP response that contains the JSON representation of books
     return HttpResponse(serializers.serialize('json', books))
+
+@csrf_exempt
+def publish_book_flutter(request):
+    try:
+        if request.user.role != 'AUTHOR':
+            return JsonResponse({'message': 'Forbidden.', 'status': 403}, status=403)
+    except:
+        return JsonResponse({'message': 'Forbidden.', 'status': 403}, status=403)
+
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+        new_book = Book.objects.create(
+            author_id = request.user,
+            book_title = data["book_title"],
+            description = data["description"],
+            book_cover_link = data["book_cover_link"]
+        )
+
+        new_book.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
+def get_book_by_author_json_flutter(request):
+    # Check if user.role is AUTHOR, return FORBIDDEN if not
+    try:
+        if request.user.role != 'AUTHOR':
+            return JsonResponse({'message': 'Forbidden.', 'status': 403}, status=403)
+    except:
+        return JsonResponse({'message': 'Forbidden.', 'status': 403}, status=403)
+    
+    # Get author's books
+    books = Book.objects.filter(author_id=request.user.id)
+    booksJson = serializers.serialize("json", books)
+
+    return JsonResponse({"status": "success", "books" : booksJson}, status=200)
+    
