01952bac143d272155fe9e7fc965890469ac5da3
Arxeno <rmfbkw04@gmail.com>
2024-03-19 05:48:32 +0700

    fix: v1 url

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
-from django.urls import URLPattern, URLResolver
+from django.urls import URLPattern, URLResolver, include, path
 
 
-urlpatterns: list[URLPattern | URLResolver] = []
+urlpatterns: list[URLPattern | URLResolver] = [
+    path('v1/', include(('api.v1.urls', 'v1'), namespace='v1'))
+]
