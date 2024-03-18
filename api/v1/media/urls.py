5dd47a853c3d9250548a238e824579a503e42199
Arxeno <rmfbkw04@gmail.com>
2024-03-19 05:55:19 +0700

    fix: delete media urls and add v1 urls

--- api/v1/media/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import URLPattern, URLResolver
-
-
-urlpatterns: list[URLPattern | URLResolver] = []
--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,4 +1,13 @@
-from django.urls import URLPattern, URLResolver
+from django.urls import URLPattern, URLResolver, path, include
 
 
-urlpatterns: list[URLPattern | URLResolver] = []
+urlpatterns: list[URLPattern | URLResolver] = [
+    path('auth/', include(('api.v1.authentication.urls', 'auth'), namespace='v1')),
+    path('bookmark/', include(('api.v1.bookmark.urls', 'bookmark'), namespace='bookmark')),
+    path('event/', include(('api.v1.event.urls', 'event'), namespace='event')),
+    path('forum/', include(('api.v1.forum.urls', 'forum'), namespace='forum')),
+    path('main/', include(('api.v1.main.urls', 'main'), namespace='main')),
+    path('event/', include(('api.v1.event.urls', 'event'), namespace='event')),
+    path('publish/', include(('api.v1.publish.urls', 'publish'), namespace='publish')),
+    path('review/', include(('api.v1.review.urls', 'review'), namespace='review')),
+]
